5186a3ead25fdace077c4c2d4fe57ea832284c7a
Denys Shevchuk <danbogameden@gmail.com>
2024-03-05 21:55:42 +0200

    modify program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,30 @@
-﻿var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+﻿namespace DPGI1
+{
 
-app.MapGet("/", () => "Hello World!");
+    /// <summary>
+    /// Creating class Hello for greeting
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Hello hello = new Hello("Deny");
+            hello.Greeting();
+        }
+    }
 
-app.Run();
+    class Hello
+    {
+        private String Name { get; set; }
+        public Hello(String Name)
+        {
+            this.Name = Name;
+        }
+        public void Greeting()
+        {
+            Console.WriteLine($"Hello {Name} !");
+        }
 
+    }
+}
